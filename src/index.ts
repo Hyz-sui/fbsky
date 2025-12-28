b01cbd2c6b7700d656502c9da49ae152b72f2c33
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2025-12-28 20:48:38 +0900

    feat: Implement language enforcement via UI toggle and URL parameter for generated links

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,25 @@ import { errorResponseService } from "./services/error-response-service";
 export default {
 
     async fetch(request, env, ctx): Promise<Response> {
-        const language = request.headers.get('Accept-Language');
+        const requestUrl = new URL(request.url);
+        // UA is browser or wants to be treated as browser
+        const isBrowser = (() => {
+            if (request.headers.has('sec-fetch-mode')) {
+                return true;
+            }
+            const userAgent = request.headers.get('user-agent');
+            return userAgent && userAgent.includes('Mozilla');
+        })();
+        const language = (() => {
+            // If browser, use Accept-Language header
+            if (isBrowser) {
+                return request.headers.get('Accept-Language');
+            }
+            // Expects Twitterbot or other bots
+            const langParameter = requestUrl.searchParams.get('lang');
+            return langParameter || request.headers.get('Accept-Language');
+        })();
+        // 優先順位の判定はサボる
         const isJapanese = language?.includes('ja');
         const clientEnvironment: ClientEnvironment = {
             language: isJapanese ? 'ja' : 'en',
